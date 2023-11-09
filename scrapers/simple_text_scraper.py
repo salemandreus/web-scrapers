c7a788f4879baa59ed5d5731bccb2a9001ad74d8
Salemandreus <salemandreus@gmail.com>
2023-11-09 16:00:44 +0200

    simple text scraper takes and output file path

--- scrapers/simple_text_scraper.py
+++ scrapers/simple_text_scraper.py
@@ -1,14 +1,14 @@
 """
 A simple web scraper that writes text to a file
-Input the site url.
+Input the site url and a .txt file path to write to.
 """
 
 import requests
 
-def getScrapedText(url):
+def getScrapedText(url, output_file_path):
     res = requests.get(url)
     res.raise_for_status()
 
-    outputFile = open('output.txt', 'wb') #output to a file in write binary mode to maintain unicode encoding
+    outputFile = open(output_file_path, 'wb') #output to a file in write binary mode to maintain unicode encoding
     for chunk in res.iter_content(100000):
         outputFile.write(chunk)
